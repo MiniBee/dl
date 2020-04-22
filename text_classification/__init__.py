98399af6e9f1efb931b61f2ae3c00f83283eb5e9
裴宏悦 <hongyue.pei@yiducloud.cn>
2020-04-22 20:37:23 +0800

    AAA

---
+++ text_classification/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @author: hongyue.pei
+# @file: __init__.py.py
+# @time: 2020/4/20 上午9:46
+# @desc:
+
---
+++ text_classification/han.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @author: hongyue.pei
+# @file: han.py
+# @time: 2020/4/20 上午9:46
+# @desc:
+
+
+
+
+
+
+
