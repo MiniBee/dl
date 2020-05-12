7bc97225f46ba38633b1dc458b47bb169c11d524
裴宏悦 <hongyue.pei@yiducloud.cn>
2020-05-12 21:18:12 +0800

    ai studio

---
+++ ai_studio/gcn.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @author: hongyue.pei
+# @file: gcn.py
+# @time: 2020/5/12 下午5:38
+# @desc:
+
+import tensorflow as tf
+
+
+class GCN(tf.keras.layers.Layer):
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
